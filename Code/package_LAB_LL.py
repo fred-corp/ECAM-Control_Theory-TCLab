2dfef652a4ea81bfa33918c11001f68290cfdc9a
fred-corp <freddruppel@gmail.com>
2022-03-23 13:12:44 +0100

    put Lead-Lad into same file as PID

--- Code/package_LAB_LL.py
+++ Code/package_LAB_LL.py
@@ -1,39 +1,34 @@
-import numpy as np
-
-import matplotlib.pyplot as plt
-from IPython.display import display, clear_output
-
 def LeadLag_System(MV,Kp,Tlag,Tlead,Ts,PV,PVInit=0,method='EBD'):
-    
-    """
-    The function "FO_RT" needs to be included in a "for or while loop".
-    
-    :MV: input vector
-    :Kp: process gain
-    :Tlag: lag time constant [s]
-    :Tlead: lead time constant [s]
-    :Ts: sampling period [s]
-    :PV: output vector
-    :PVInit: (optional: default value is 0)
-    :method: discretisation method (optional: default value is 'EBD')
-        EBD: Euler Backward difference
-        EFD: Euler Forward difference
-        TRAP: Trapezoïdal method
-    
-    The function "FO_RT" appends a value to the output vector "PV".
-    The appended value is obtained from a recurrent equation that depends on the discretisation method.
-    """    
-    
-    if (Tlag != 0):
-        K = Ts/Tlag
-        if len(PV) == 0:
-            PV.append(PVInit)
-        else: # MV[k+1] is MV[-1] and MV[k] is MV[-2]
-            if method == 'EBD':
-                PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*((1+Tlead/Ts)*MV[-1]-(Tlead/Ts)*MV[-2]))
-            elif method == 'EFD':
-                PV.append((1-K)*PV[-1] + K*Kp*((Tlead/Ts)*MV[-1]+(1-(Tlead/Ts))*MV[-2]))
-            else:
-                PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*((1+Tlead/Ts)*MV[-1]-(Tlead/Ts)*MV[-2]))
-    else:
-        PV.append(Kp*MV[-1])
+  
+  """
+  The function "FO_RT" needs to be included in a "for or while loop".
+  
+  :MV: input vector
+  :Kp: process gain
+  :Tlag: lag time constant [s]
+  :Tlead: lead time constant [s]
+  :Ts: sampling period [s]
+  :PV: output vector
+  :PVInit: (optional: default value is 0)
+  :method: discretisation method (optional: default value is 'EBD')
+    EBD: Euler Backward difference
+    EFD: Euler Forward difference
+    TRAP: Trapezoïdal method
+  
+  The function "FO_RT" appends a value to the output vector "PV".
+  The appended value is obtained from a recurrent equation that depends on the discretisation method.
+  """    
+  
+  if (Tlag != 0):
+    K = Ts/Tlag
+    if len(PV) == 0:
+      PV.append(PVInit)
+    else: # MV[k+1] is MV[-1] and MV[k] is MV[-2]
+      if method == 'EBD':
+        PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*((1+Tlead/Ts)*MV[-1]-(Tlead/Ts)*MV[-2]))
+      elif method == 'EFD':
+        PV.append((1-K)*PV[-1] + K*Kp*((Tlead/Ts)*MV[-1]+(1-(Tlead/Ts))*MV[-2]))
+      else:
+        PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*((1+Tlead/Ts)*MV[-1]-(Tlead/Ts)*MV[-2]))
+  else:
+    PV.append(Kp*MV[-1])
--- Code/package_LAB_PID.py
+++ Code/package_LAB_PID.py
@@ -1,7 +1,38 @@
-# Libs
-
 # Lead-Lag
-
+def LeadLag_System(MV,Kp,Tlag,Tlead,Ts,PV,PVInit=0,method='EBD'):
+  
+  """
+  The function "FO_RT" needs to be included in a "for or while loop".
+  
+  :MV: input vector
+  :Kp: process gain
+  :Tlag: lag time constant [s]
+  :Tlead: lead time constant [s]
+  :Ts: sampling period [s]
+  :PV: output vector
+  :PVInit: (optional: default value is 0)
+  :method: discretisation method (optional: default value is 'EBD')
+    EBD: Euler Backward difference
+    EFD: Euler Forward difference
+    TRAP: Trapezoïdal method
+  
+  The function "FO_RT" appends a value to the output vector "PV".
+  The appended value is obtained from a recurrent equation that depends on the discretisation method.
+  """    
+  
+  if (Tlag != 0):
+    K = Ts/Tlag
+    if len(PV) == 0:
+      PV.append(PVInit)
+    else: # MV[k+1] is MV[-1] and MV[k] is MV[-2]
+      if method == 'EBD':
+        PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*((1+Tlead/Ts)*MV[-1]-(Tlead/Ts)*MV[-2]))
+      elif method == 'EFD':
+        PV.append((1-K)*PV[-1] + K*Kp*((Tlead/Ts)*MV[-1]+(1-(Tlead/Ts))*MV[-2]))
+      else:
+        PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*((1+Tlead/Ts)*MV[-1]-(Tlead/Ts)*MV[-2]))
+  else:
+    PV.append(Kp*MV[-1])
 
 # Constrain
 def constrain(val, min_val, max_val):
@@ -75,13 +106,4 @@ def PID(PV, SP, MV, Ts, Kc, Ti, Td, alpha, approximationType, man=False, manMV=[
   output = {"MV": MV, "MVp": MVp, "MVi": MVi, "MVd": MVd, "E": E}
   return output
 
-
-# FF
-def FF(P, D, gamma):
-  # P & D are dictionaries with parameters : K, T1, T2, Theta
-
-  gain = - D["K"] / P["K"]
-
-
-
-  pass
+# IMC Tuning
