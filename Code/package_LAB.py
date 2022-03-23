86c7df7519972ba727d39f1ccf83fcc688a25c35
fred-corp <freddruppel@gmail.com>
2022-03-23 14:13:30 +0100

    changed leadlag function to return value instead of altering incoming data

--- Code/package_LAB.py
+++ Code/package_LAB.py
@@ -20,19 +20,27 @@ def LeadLag_RT(MV,Kp,Tlag,Tlead,Ts,PV,PVInit=0,method='EBD'):
   The appended value is obtained from a recurrent equation that depends on the discretisation method.
   """    
   
+
+
+  try :
+    PV_previous = PV[-1]
+  except :
+    PV_previous = PVInit
+    return PV_previous
+
   if (Tlag != 0):
     K = Ts/Tlag
-    if len(PV) == 0:
-      PV.append(PVInit)
-    else: # MV[k+1] is MV[-1] and MV[k] is MV[-2]
-      if method == 'EBD':
-        PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*((1+Tlead/Ts)*MV[-1]-(Tlead/Ts)*MV[-2]))
-      elif method == 'EFD':
-        PV.append((1-K)*PV[-1] + K*Kp*((Tlead/Ts)*MV[-1]+(1-(Tlead/Ts))*MV[-2]))
-      else:
-        PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*((1+Tlead/Ts)*MV[-1]-(Tlead/Ts)*MV[-2]))
+    # MV[k+1] is MV[-1] and MV[k] is MV[-2]
+    if method == 'EBD':
+      PVout = (1/(1+K))*PV_previous + (K*Kp/(1+K))*((1+Tlead/Ts)*MV[-1]-(Tlead/Ts)*MV[-2])
+    elif method == 'EFD':
+      PVout = (1-K)*PV_previous + K*Kp*((Tlead/Ts)*MV[-1]+(1-(Tlead/Ts))*MV[-2])
+    else:
+      PVout = (1/(1+K))*PV_previous + (K*Kp/(1+K))*((1+Tlead/Ts)*MV[-1]-(Tlead/Ts)*MV[-2])
   else:
-    PV.append(Kp*MV[-1])
+    PVout = Kp*MV[-1]
+
+  return PVout
 
 # Constrain
 def constrain(val, min_val, max_val):
